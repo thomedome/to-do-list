61d3aadc838588587048b20d8bd006a9c3f8345d
thomedome <tom@empire-computers.co.uk>
2025-06-28 22:43:20 +0100

    testing list

--- list.php
+++ list.php
@@ -1,3 +1,16 @@
 <?php
-    include('connection.php')
+    include('connection.php');
+    $query = $_GET['q'];
+
+    $sql = "nil";
+    $result = "nil";
+
+    if ($query == "") {
+        $sql = "SELECT * FROM toDoList";
+        $result = $con->query($sql);
+    }
+
+    foreach ($result as $item) {
+        echo $item->item . "<br>" . $item->id;
+    }
 ?>
