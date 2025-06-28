63ba8370b7d7760ca412181a39b6ab6ef24b16b1
thomedome <tom@empire-computers.co.uk>
2025-06-28 22:19:18 +0100

    the beginning.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-
+include('connection.php')
 ?>
+
+<html>
+    <head>
+        <link href="./css/custom.css" rel="stylesheet"> <!-- custom css -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"> <!-- bootstrap -->
+    </head>
+    <body>
+        <br><br><br>
+        <h1 id="title" class="text-center">To-Do List</h1>
+        <h3 class="text-center">by thomedome</h3>
+    </body>
+</html>
