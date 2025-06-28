5832d6df5d02589ced4a2c642dbcbe5d0fc9ff0e
thomedome <tom@empire-computers.co.uk>
2025-06-28 22:15:28 +0100

    connection to db

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $env = parse_ini_file('.env');
+
+    $hostname = $env['db_hostname'];
+    $user = $env['db_user'];
+    $password = $env['db_pass'];
+    $database = $env['db_db'];
+    
+    $con = mysqli_connect($hostname, $user, $password, $database);
+    if (mysqli_connect_errno()) {
+        die("Failed to connect to MySQL:" . mysqli_connect_error());
+    }
+?>
